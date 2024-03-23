0ee19a4545c982714b771afc256c5d559430ad0c
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-03-23 19:41:46 +0530

    Update Container.jsx

--- Container.jsx
+++ Container.jsx
@@ -2,7 +2,7 @@ function Container(props)
 {
  return (
  <>
- <div class="w-[400px] h-[300px] border-3  bg-black m-[auto]">{props.children}</div>
+ <div class="w-[400px] h-[300px] border-3  bg-black m-[auto]">{props.children}</div> 
       
     </>
  )
