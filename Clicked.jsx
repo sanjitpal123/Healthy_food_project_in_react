3a368297b6a8e7c650a64ee483445c2cbd40adf1
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-05-17 21:03:30 +0530

    Update Clicked.jsx

--- Clicked.jsx
+++ Clicked.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Heading from './Heading';
 import './App.css';
 import ItemBox from './Itembox';
-import { Container } from 'react-dom';
+import { Container } from 'react-dom'; 
 
 function App() {
   return ( 
