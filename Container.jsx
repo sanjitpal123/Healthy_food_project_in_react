262905afe746e26dc59256c6dbfc9f62a391c10c
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-06-02 09:46:26 +0530

    Update Container.jsx

--- Container.jsx
+++ Container.jsx
@@ -4,7 +4,7 @@ function Container(props)
  <>
  <div class="w-[400px] h-[300px] border-3  bg-black m-[auto]">{props.children}</div>   
            
-    </>
+    </> 
  ) 
 }
 export default Container
