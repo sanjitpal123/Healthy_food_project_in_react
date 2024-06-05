6b6739ee9920fb7c765caaa71a67d5537d97b6f5
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-06-05 20:40:36 +0530

    Update Heading.jsx

--- Heading.jsx
+++ Heading.jsx
@@ -1,7 +1,7 @@
 function Heading()
 {
     return ( 
-        <h1 class="font-bold text-center text-white text-3xl">List of all Healthy food</h1>        
+        <h1 class="font-bold text-center text-white text-3xl">List of all Healthy food</h1>         
     )    
 } 
 export default Heading;     
